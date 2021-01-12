6796455e203a693cdddbd8717e457f0a0b8f5363
Mat Seto <seanseto@gmail.com>
2021-01-12 11:26:21 -0800

    add redux devtools extension

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,16 +1,12 @@
-import {
-  applyMiddleware,
-  combineReducers,
-  createStore,
-  Store,
-  compose,
-} from "redux"
+import { applyMiddleware, combineReducers, createStore } from "redux"
 import thunk from "redux-thunk"
 
 import { networksReducer } from "./networks/networksReducer"
 import { INetworksState } from "./networks/networkTypes"
 import { authReducer } from "./auth/authReducer"
 import { IAuthState } from "./auth/authTypes"
+
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly"
 
 export interface IApplicationState {
   networks: INetworksState
@@ -24,10 +20,5 @@ const rootReducer = combineReducers<IApplicationState>({
 
 export const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    // TODO: Remove in Production
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-      (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
+  composeWithDevTools(applyMiddleware(thunk)),
 )
