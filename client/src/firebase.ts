c3bb6c1e349f047c129bf83945e2a4f2c4af7e9d
Mat Seto <seanseto@gmail.com>
2021-01-20 12:20:38 -0800

    Implement storage functions

--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -1,13 +1,75 @@
 import firebase from "firebase"
 import "firebase/auth"
 import "firebase/firestore"
+import "firebase/storage"
+import { v4 as uuid } from "uuid"
 import { firebaseConfig } from "./.firebaseConfig"
 
 // ==- INITIALIZATION -== //
 const app = firebase.initializeApp(firebaseConfig)
 
-// ==- FIRESTORE (DATABASE) SETUP -== //
+// ==- FIRESTORE (DATABASE) -== //
 export const db = firebase.firestore()
 
-// ==- AUTHENTICATION SETUP -== //
+// ==- AUTHENTICATION -== //
 export const auth = app.auth()
+
+// ==- STORAGE -== //
+const THUMBNAILS_PATH = "thumbnails"
+const storage = app.storage()
+
+/**
+ * Upload a thumbnail to Firebase storage
+ * @param thumbnail File or external link to the thumbnail
+ */
+export async function uploadThumbnail(thumbnail: File | string) {
+  const path = generateThumbnailPath()
+  if (!path) return
+
+  const ref = storage.ref(path)
+
+  if (typeof thumbnail === "string") {
+    const result = await ref.putString(thumbnail)
+    console.log(result)
+    console.log("Saved link.")
+  } else {
+    const result = await ref.put(thumbnail)
+    console.log(result)
+    console.log("Uploaded link.")
+  }
+
+  const url = await ref.getDownloadURL()
+  // TODO: return result
+  console.log(url)
+}
+
+/**
+ * Generate a thumbnail storage path based on the current logged in user's id
+ */
+export function generateThumbnailPath(): string | null {
+  if (!auth.currentUser) {
+    console.error("You must be logged in to upload thumbnails.")
+    return null
+  }
+
+  const userId = auth.currentUser.uid
+  const thumbnailId = uuid()
+  const path = `${THUMBNAILS_PATH}/${userId}/${thumbnailId}`
+  return path
+}
+
+/* get the list of all thumbnail urls uploadaed by the current user  */
+// TODO: return results
+export async function getCurrentUserThumbnails() {
+  if (!auth.currentUser) {
+    console.error("You must be logged in to view uploaded thumbnails.")
+    return
+  }
+
+  const userId = auth.currentUser.uid
+  const path = `${THUMBNAILS_PATH}/${userId}`
+  const thumbnails = await storage.ref(path).list()
+  const urls = thumbnails.items.map((item) => item.getDownloadURL())
+  const results = await Promise.all(urls)
+  console.log(results)
+}
