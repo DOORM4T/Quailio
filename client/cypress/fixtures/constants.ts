14b16700c6c621f88700f9ebe1f67e6b8d0fcdc4
Mat Seto <seanseto@gmail.com>
2021-02-01 12:19:13 -0800

    add constants to fixtures

---
+++ client/cypress/fixtures/constants.ts
@@ -0,0 +1,5 @@
+export const BASE_URL = Cypress.config().baseUrl || ""
+export const TEST_EMAIL = "test@example.com"
+export const TEST_PASSWORD = "test123"
+export const CONFIRM_DELETE_ACCOUNT_MESSAGE =
+  "Are you sure you want to delete your account? This action cannot be reversed."
