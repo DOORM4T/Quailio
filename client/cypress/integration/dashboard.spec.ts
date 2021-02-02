4b55dd87481723627c0a03354069ecf4e936ee53
Mat Seto <seanseto@gmail.com>
2021-02-02 13:05:55 -0800

    Test adding 2 people

--- client/cypress/integration/dashboard.spec.ts
+++ client/cypress/integration/dashboard.spec.ts
@@ -1,13 +1,18 @@
+import { IApplicationState } from "../../src/store/store"
 import {
   BASE_URL,
   CONFIRM_DELETE_ACCOUNT_MESSAGE,
   TEST_EMAIL,
   TEST_PASSWORD,
 } from "../fixtures/constants"
+
 const FIRST_NETWORK_NAME = "First Test Network"
 const SECOND_NETWORK_NAME = "Second Test Network"
 
-describe("Dashboard", () => {
+const FIRST_PERSON_NAME = "Luke Skywalker"
+const SECOND_PERSON_NAME = "Darth Vader"
+
+describe("Setup", () => {
   it("Sends a registered user to /dashboard", () => {
     cy.visit("/register")
       .get("#email-field")
@@ -21,16 +26,17 @@ describe("Dashboard", () => {
       .url()
       /* Successful registration should send the user to the Dashboard route */
       .should("equal", `${BASE_URL}/dashboard`)
-      .wait(5000) // wait for the user doc to be created
+      .wait(10_000) // wait for the user doc to be created
   })
+})
 
+describe("Dashboard", () => {
   it("Creates a network and selects it", () => {
-    cy.visit("/dashboard", {
-      onBeforeLoad(win) {
+    cy.window()
+      .then((win) => {
         /* Stub the output of "Create Network" prompt  */
         cy.stub(win, "prompt").returns(FIRST_NETWORK_NAME)
-      },
-    })
+      })
       .get("#create-network-button")
       .click()
       .window()
@@ -70,7 +76,37 @@ describe("Dashboard", () => {
       .get("#select-network-dropbutton")
       .should("contain.text", FIRST_NETWORK_NAME)
   })
+})
 
+describe("People", () => {
+  it("Adds a person", () => {
+    cy.window()
+      /* Stub the Add Person prompt to return the first test person name */
+      .then((win) => cy.stub(win, "prompt").returns(FIRST_PERSON_NAME))
+      /* Click the Add Person button, which prompts the user */
+      .get("#add-person-button")
+      .click()
+      /* Wait for the person menu to display a person */
+      .wait(2000)
+      .get("#person-menu > li")
+      .should("have.length", 1)
+  })
+
+  it("Adds a second person", () => {
+    cy.window()
+      /* Stub the Add Person prompt to return the first test person name */
+      .then((win) => cy.stub(win, "prompt").returns(SECOND_PERSON_NAME))
+      /* Click the Add Person button, which prompts the user */
+      .get("#add-person-button")
+      .click()
+      /* Wait for the person menu to display a person */
+      .wait(2000)
+      .get("#person-menu > li")
+      .should("have.length", 2)
+  })
+})
+
+describe("Cleanup", () => {
   it("Deletes the user account", () => {
     cy.on("window:confirm", (str) => {
       expect(str).to.equal(CONFIRM_DELETE_ACCOUNT_MESSAGE)
