08d28bc653035bebb9a0cf930f557f6a26ef291b
Mat Seto <seanseto@gmail.com>
2021-02-21 10:12:13 -0800

    Allow connections without database interaction

--- client/src/store/networks/actions/connectPeople.ts
+++ client/src/store/networks/actions/connectPeople.ts
@@ -27,30 +27,49 @@ export const connectPeople = (
     p2Reason: string
   },
 ): AppThunk => {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
     dispatch(setNetworkLoading(true))
 
     const { p1Id, p2Id, p1Reason = "", p2Reason = "" } = relationship
 
     try {
-      /* Database updates */
-      /* Ensure each person exists in the network */
-      const network = (
-        await networksCollection.doc(networkId).get()
-      ).data() as INetwork
-      const hasP1 = network.personIds.some((id) => id === p1Id)
-      const hasP2 = network.personIds.some((id) => id === p2Id)
-      if (!hasP1 || !hasP2)
-        throw new Error("One or both persons do not exist in that network.")
+      let p1Data: IPerson | undefined
+      let p2Data: IPerson | undefined
+      let willUpdateDatabase = false
 
-      /* Get the Firestore documents for each Person */
-      const p1Doc = peopleCollection.doc(p1Id)
-      const p2Doc = peopleCollection.doc(p2Id)
+      /* Database changes if user is authenticated */
+      if (getState().auth.userId) {
+        /* Database updates */
+        /* Ensure each person exists in the network */
+        const network = (
+          await networksCollection.doc(networkId).get()
+        ).data() as INetwork
+        const hasP1 = network.personIds.some((id) => id === p1Id)
+        const hasP2 = network.personIds.some((id) => id === p2Id)
+        if (!hasP1 || !hasP2)
+          throw new Error("One or both persons do not exist in that network.")
 
-      /* Get the document data for each person */
-      const p1Data: IPerson = (await p1Doc.get()).data() as IPerson
-      const p2Data: IPerson = (await p2Doc.get()).data() as IPerson
+        /* Get the Firestore documents for each Person */
+        const p1Doc = peopleCollection.doc(p1Id)
+        const p2Doc = peopleCollection.doc(p2Id)
 
+        /* Get the document data for each person */
+        p1Data = (await p1Doc.get()).data() as IPerson
+        p2Data = (await p2Doc.get()).data() as IPerson
+
+        /* Will update the database after the connected user data is created */
+        willUpdateDatabase = true
+      } else {
+        /* User isn't authenticated -- just update local state */
+        const people = getState().networks.currentNetwork?.people
+        if (!people) throw new Error("No current state to find people from")
+
+        p1Data = people.find((p) => p.id === p1Id)
+        p2Data = people.find((p) => p.id === p2Id)
+        if (!p1Data || !p2Data) throw new Error("One or more persons not found")
+      }
+
+      /* Create connected user data */
       /* Copy existing relationships and add the new relationship for each person */
       const updatedP1Rels: IRelationships = {
         ...p1Data.relationships,
@@ -61,10 +80,18 @@ export const connectPeople = (
         [p1Id]: [p2Reason, p1Reason],
       }
 
-      /* Update just the "relationships" field in each Person document */
-      await p1Doc.update({ relationships: updatedP1Rels })
-      await p2Doc.update({ relationships: updatedP2Rels })
+      /* Updated person data fetched from the database? Then update the database with the new data. */
+      if (willUpdateDatabase) {
+        /* Update just the "relationships" field in each Person document */
+        await peopleCollection
+          .doc(p1Id)
+          .update({ relationships: updatedP1Rels })
+        await peopleCollection
+          .doc(p2Id)
+          .update({ relationships: updatedP2Rels })
+      }
 
+      /* Format updated person data for use by the reducer to update global state */
       const updatedP1Data: IPerson = { ...p1Data, relationships: updatedP1Rels }
       const updatedP2Data: IPerson = { ...p2Data, relationships: updatedP2Rels }
 
--- client/src/store/networks/actions/disconnectPeople.ts
+++ client/src/store/networks/actions/disconnectPeople.ts
@@ -25,30 +25,49 @@ export const disconnectPeople = (
     p2Id: string
   },
 ): AppThunk => {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
     dispatch(setNetworkLoading(true))
 
     const { p1Id, p2Id } = relationship
 
     try {
-      /* Database updates */
-      /* Ensure each person exists in the network */
-      const network = (
-        await networksCollection.doc(networkId).get()
-      ).data() as INetwork
-      const hasP1 = network.personIds.some((id) => id === p1Id)
-      const hasP2 = network.personIds.some((id) => id === p2Id)
-      if (!hasP1 || !hasP2)
-        throw new Error("One or both persons do not exist in that network.")
+      let p1Data: IPerson | undefined
+      let p2Data: IPerson | undefined
+      let willUpdateDatabase = false
 
-      /* Get the Firestore documents for each Person */
-      const p1Doc = peopleCollection.doc(p1Id)
-      const p2Doc = peopleCollection.doc(p2Id)
+      /* User is authenticated? Use database data. */
+      if (getState().auth.userId) {
+        /* Database updates */
+        /* Ensure each person exists in the network */
+        const network = (
+          await networksCollection.doc(networkId).get()
+        ).data() as INetwork
+        const hasP1 = network.personIds.some((id) => id === p1Id)
+        const hasP2 = network.personIds.some((id) => id === p2Id)
+        if (!hasP1 || !hasP2)
+          throw new Error("One or both persons do not exist in that network.")
 
-      /* Get the document data for each person */
-      const p1Data: IPerson = (await p1Doc.get()).data() as IPerson
-      const p2Data: IPerson = (await p2Doc.get()).data() as IPerson
+        /* Get the Firestore documents for each Person */
+        const p1Doc = peopleCollection.doc(p1Id)
+        const p2Doc = peopleCollection.doc(p2Id)
 
+        /* Get the document data for each person */
+        p1Data = (await p1Doc.get()).data() as IPerson
+        p2Data = (await p2Doc.get()).data() as IPerson
+
+        /* Will update database data after the diconnected relationship data is created */
+        willUpdateDatabase = true
+      } else {
+        /* User isn't authenticated -- just update local state */
+        const people = getState().networks.currentNetwork?.people
+        if (!people) throw new Error("No current state to find people from")
+
+        p1Data = people.find((p) => p.id === p1Id)
+        p2Data = people.find((p) => p.id === p2Id)
+        if (!p1Data || !p2Data) throw new Error("One or more persons not found")
+      }
+
+      /* Create the disconnected relationship data */
       /* Copy existing relationships */
       const updatedP1Rels: IRelationships = { ...p1Data.relationships }
       const updatedP2Rels: IRelationships = { ...p2Data.relationships }
@@ -57,10 +76,18 @@ export const disconnectPeople = (
       delete updatedP1Rels[p2Id]
       delete updatedP2Rels[p1Id]
 
-      /* Update just the "relationships" field in each Person document */
-      await p1Doc.update({ relationships: updatedP1Rels })
-      await p2Doc.update({ relationships: updatedP2Rels })
+      /* Update the people docs in the database if fetched data was changed  */
+      if (willUpdateDatabase) {
+        /* Update just the "relationships" field in each Person document */
+        await peopleCollection
+          .doc(p1Id)
+          .update({ relationships: updatedP1Rels })
+        await peopleCollection
+          .doc(p2Id)
+          .update({ relationships: updatedP2Rels })
+      }
 
+      /* Format updated person data for use by the reducer to update global state */
       const updatedP1Data: IPerson = { ...p1Data, relationships: updatedP1Rels }
       const updatedP2Data: IPerson = { ...p2Data, relationships: updatedP2Rels }
 
