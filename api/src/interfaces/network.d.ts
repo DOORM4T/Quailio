895ba195212ef1eb3f342d771d6bfd1feb4c2492
Mat Seto <seanseto@gmail.com>
2021-06-29 12:26:14 -0700

    Get people data

--- api/src/interfaces/network.d.ts
+++ api/src/interfaces/network.d.ts
@@ -5,6 +5,11 @@ export interface INetwork {
   sharedProperties?: ISharedNetworkProperties
 }
 
+export interface INetworkExport {
+  name: string
+  people: IPerson[]
+}
+
 interface ISharedNetworkProperties {
   sharedId: string | null
   allowList: IAllowListUser[]
@@ -14,3 +19,29 @@ interface IAllowListUser {
   email: string
   canEdit: boolean
 }
+
+export interface IPerson {
+  id: string
+  name: string
+  relationships: IRelationships
+  thumbnailUrl?: string
+  content?: string
+  pinXY?: XYVals
+  scaleXY?: XYVals
+  isBackground?: boolean
+  isGroup?: boolean
+  backgroundColor?: string
+  textColor?: string
+  doHideNameTag?: boolean
+}
+
+type IRelationships = { [otherPersonId: string]: IRelationship }
+
+interface IRelationship {
+  reason: string
+  shape?: ConnectionShape
+}
+
+type ConnectionShape = "arrow" | "none"
+
+type XYVals = { x: number; y: number }
--- api/src/services/firebase/getNetwork.ts
+++ api/src/services/firebase/getNetwork.ts
@@ -1,11 +1,51 @@
-import { networks } from "."
-import { INetwork } from "../../interfaces/network"
+import { networks, people } from "."
+import { INetwork, INetworkExport, IPerson } from "../../interfaces/network"
 
-export async function getNetwork(networkId: string) {
-  const doc = await networks.doc(networkId).get()
-  if (!doc.exists) throw new Error("That network does not exist.")
-  const data = doc.data() as INetwork
-  if (!data.sharedProperties?.sharedId)
-    throw new Error("You are not authorized to view this network.")
-  if (data) return data
+export async function getNetwork(sharedId: string) {
+  const data = await getSharedNetworkData(sharedId)
+  const relationalPeople = await getRelationalPeople(data)
+  const sortedPeople = getSortedPeople(relationalPeople)
+
+  const network: INetworkExport = {
+    name: data.name || "Untitled Network",
+    people: sortedPeople,
+  }
+  return network
+}
+
+async function getSharedNetworkData(sharedId: string) {
+  const doc = await networks
+    .where("sharedProperties.sharedId", "==", sharedId)
+    .limit(1)
+    .get()
+  if (!doc.docs[0].exists) throw new Error("That network does not exist.")
+
+  const data = doc.docs[0].data() as INetwork
+  return data
+}
+
+async function getRelationalPeople(data: INetwork) {
+  const getPersonPromises = data.personIds
+    .map(idToPerson)
+    .filter(isNonNull) as Promise<IPerson>[]
+  return await Promise.all(getPersonPromises)
+}
+
+async function idToPerson(personId: string) {
+  const doc = await people.doc(personId).get()
+  if (!doc.exists) return null
+  return doc.data() as IPerson
+}
+
+function isNonNull(item: any | null) {
+  if (item === null) return false
+  return true
+}
+
+function getSortedPeople(peopleData: IPerson[]) {
+  // Sort people by name
+  // This prevents person data from "changing position," since they may be fetched out of order
+  return peopleData.sort((p1, p2) =>
+    p1.name.toLocaleLowerCase().localeCompare(p2.name.toLocaleLowerCase()),
+  )
 }
